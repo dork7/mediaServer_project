9c7c4fcd158689467e89855f3770c04b44291cb4
dork7 <hamzameh122@gmail.com>
2020-03-30 22:27:23 +0500

    30-03-2020 bak

--- push.sh
+++ push.sh
@@ -2,3 +2,5 @@ git add --all
 date=$(date '+%d-%m-%Y')
 git commit -m "${date} bak"
 git push -u origin master
+
+#touch .gitignore && echo "electron_wp\node_modules" >> .gitignore && git rm -r --cached node_modules ; git status # to remove node modules
