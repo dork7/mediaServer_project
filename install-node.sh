0ba2232068caf91d8c903416e74a4d62a7205549
hamza <hamza@hamzas-MacBook-Pro.local>
2026-03-13 12:29:11 +0400

    13-03-2026 bak

--- install-node.sh
+++ install-node.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Installing Node.js 20 on Raspberry Pi..."
+echo "Installing Node.js 22 on Raspberry Pi..."
 
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 echo ""
